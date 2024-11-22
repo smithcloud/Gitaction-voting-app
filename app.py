174aaa04e8b4e26ac25f26a2a0d2aa327e995b29
Sungguk Hong <144807463+smithcloud@users.noreply.github.com>
2024-11-22 20:40:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
 
 def get_redis():
-    if not hasattr(g, 'redis.meoyyu.ng.0001.apn2.cache.amazonaws.com'):
-        g.redis = Redis(host="redis.meoyyu.ng.0001.apn2.cache.amazonaws.com", db=0, socket_timeout=5)
+    if not hasattr(g, 'voting-app-redis.meoyyu.ng.0001.apn2.cache.amazonaws.com'):
+        g.redis = Redis(host="voting-app-redis.meoyyu.ng.0001.apn2.cache.amazonaws.com", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
